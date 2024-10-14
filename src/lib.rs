f774991103f2900650da0c8ad3cb535521babc7a
Mearkatz <drquinsey@gmail.com>
2024-10-14 00:21:08 -0400

    digits are now only a single byte

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,16 @@
 //! Traits for iterating over the digits of any primitive integer or float
 
+use default_impl::default_impl;
 use traits::{Digits, FloatDigits};
 
 pub mod traits;
-
-/// Expands to the default implementation of a trait on all the types provided
-macro_rules! default_impl {
-    ($tr: ty,$($x: ty),*) => {
-        $(
-            impl $tr for $x {}
-        )*
-    };
-}
 
 /// Generates an implementation of Digits for `i*` using the corresponding `u*`'s implementation
 macro_rules! piggyback {
     ($($x: ty),*) => {
         $(
             impl Digits for $x {
-                fn digits(&self) -> Box<[u32]> {
+                fn digits(&self) -> Box<[u8]> {
                     self.unsigned_abs().digits()
                 }
             }
@@ -27,12 +19,14 @@ macro_rules! piggyback {
 }
 
 /// Unsafely casts the characters in `x`'s string representation to u32's.
-/// Should only be used on unsigned or positive numeric types.
+/// # Safety
+/// Only use if `x`'s String representation only contains only digits in base 10
 #[inline(always)]
-pub fn stringable_to_digits<T: ToString>(x: T) -> Box<[u32]> {
+unsafe fn stringable_to_digits<T: ToString>(x: T) -> Box<[u8]> {
     x.to_string()
         .chars()
-        .map(|c| unsafe { c.to_digit(10).unwrap_unchecked() })
+        .map(|c| c.to_digit(10).unwrap())
+        .map(|x| x as u8)
         .collect()
 }
 
@@ -40,17 +34,17 @@ default_impl!(Digits, u8, u16, u32, u64, u128, usize);
 piggyback!(i8, i16, i32, i64, i128, isize);
 
 impl FloatDigits for f64 {
-    fn digits_left_of_dot(&self) -> Box<[u32]> {
+    fn digits_left_of_dot(&self) -> Box<[u8]> {
         if self.is_finite() {
-            stringable_to_digits(self.trunc())
+            unsafe { stringable_to_digits(self.trunc()) }
         } else {
             Box::new([]) // Infinity & NaN have no digits
         }
     }
 
-    fn digits_right_of_dot(&self) -> Box<[u32]> {
+    fn digits_right_of_dot(&self) -> Box<[u8]> {
         if self.is_finite() {
-            stringable_to_digits(self.fract())
+            unsafe { stringable_to_digits(self.fract()) }
         } else {
             Box::new([]) // Infinity & NaN have no digits
         }
--- src/traits.rs
+++ src/traits.rs
@@ -6,20 +6,20 @@ pub trait Digits: Display {
     /// Returns the digits of a primitive integer type,
     /// like a u8, or i16.
     /// The sign is not included in the returned digits.
-    fn digits(&self) -> Box<[u32]> {
-        stringable_to_digits(self)
+    fn digits(&self) -> Box<[u8]> {
+        unsafe { stringable_to_digits(self) }
     }
 }
 
 pub trait FloatDigits {
     /// Digits left of the decimal point
-    fn digits_left_of_dot(&self) -> Box<[u32]>;
+    fn digits_left_of_dot(&self) -> Box<[u8]>;
 
     /// Digits right of the decimal point
-    fn digits_right_of_dot(&self) -> Box<[u32]>;
+    fn digits_right_of_dot(&self) -> Box<[u8]>;
 
     /// Digits left of the decimal point, and right of the decimal point in that order.
-    fn digits_left_then_right_of_dot(&self) -> [Box<[u32]>; 2] {
+    fn digits_left_then_right_of_dot(&self) -> [Box<[u8]>; 2] {
         [self.digits_left_of_dot(), self.digits_right_of_dot()]
     }
 }
